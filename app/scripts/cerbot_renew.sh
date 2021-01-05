5b0eea073721a062937e28f83775e364e78315c4
flower <flower@k1024.de>
2021-01-05 12:49:32 +0100

    wrong nginx folder
    wrong nginx folder

--- app/scripts/cerbot_renew.sh
+++ app/scripts/cerbot_renew.sh
@@ -6,6 +6,6 @@ handleERR() {
 set -e
 trap 'handleERR $LINENO' ERR
 
-certbot renew "$CERTBOT_ARGS" -q --non-interactive --nginx-server-root /home/nginx --logs-dir /tmp/letsencrypt/logs --work-dir /tmp/letsencrypt/work
+certbot renew "$CERTBOT_ARGS" -q --non-interactive --nginx-server-root /tmp/nginx --logs-dir /tmp/letsencrypt/logs --work-dir /tmp/letsencrypt/work
 
 nginx -s reload
